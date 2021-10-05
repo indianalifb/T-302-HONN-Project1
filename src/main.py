5e759b18f441c261aae1f5a4870af35c7b10c231
Indiana Lif Bergsteinsdottir <indianalifb@Indianas-MacBook-Pro.local>
2021-10-05 17:56:04 +0000

    text files

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import random
+
+class Hangman:
+    def main_menu(self):
+        print('-----------------------Hangman-------------------------')
+        print("**                                                   **")
+        print("**                                                   **")
+        print("**                                                   **")
+        print("**                                                   **")
+        print("**                                                   **")
+        print("**                                                   **")
+        print("**                                                   **")
+        print("**                                                   **")
+        print("**                                                   **")
+        print("**                                                   **")
+        print('-------------------------------------------------------')
+
+
+
+
+if __name__ == '__main__':
+    game = Hangman()
+    game.main_menu()
