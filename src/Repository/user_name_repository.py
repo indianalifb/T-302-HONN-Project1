da8053adeb8fdb92f8a89a4f7d7e69cfe3fd33e7
Thelma Karen Jónsdóttir <thelmakarenjonsdottir@Thelmas-MacBook-Pro.local>
2021-10-08 16:48:07 +0000

    validator for username

--- src/Repository/user_name_repository.py
+++ src/Repository/user_name_repository.py
@@ -1,14 +1,19 @@
 from db_connection import DbConnection
 import psycopg2
+import username_validator
 
 class UserNameRepository:
-    def __init__(self, connection: DbConnection):
+    def __init__(self, connection: DbConnection, validator = username_validator):
         self.__connection = connection
+        self.validator = validator
 
     def save_username(self, username):
-        self.__connection.execute(
-        "INSERT INTO Player (USERNAME) VALUES '{username}'")
-        self.__connection.commit()
+        if self.validator:
+            self.__connection.execute(
+            "INSERT INTO Player (USERNAME) VALUES '{username}'")
+            self.__connection.commit()
+        else:
+            None
 
     def get_username(self, username):
         usernamedb = self.__connection.execute(
--- src/username_validator.py
+++ src/username_validator.py
@@ -4,7 +4,7 @@ class UsernameValidator:
     def validate(self, username: str) -> bool:
         pass
     # TODO: Validate-a að nafnið er i gagnagrunninum
-
+        return len(username) < 15
         
 
 
