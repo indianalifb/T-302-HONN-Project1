725585e963b78d7dd89d8d4f7a552dcec9a0fd02
Thelma Karen Jónsdóttir <thelmakarenjonsdottir@Thelmas-MacBook-Pro.local>
2021-10-10 23:59:08 +0000

    add pants

---
+++ src/merch/pants.py
@@ -0,0 +1,12 @@
+
+
+from merch.Imerch import Imerch
+from merch.Imerch import Imerch
+
+class Pants(Imerch):
+    def getDescription(self):
+        return "Pants"
+
+    def cost(self):
+        return 4000
+
