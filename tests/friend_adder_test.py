31229f20d77680c75b25ad9a2f667ae966aaa80c
Toggi <thorgilshjalmarsson@gmail.com>
2021-10-13 12:10:53 +0000

    Friend adder test

---
+++ tests/friend_adder_test.py
@@ -0,0 +1,17 @@
+from src.friend_adder import FriendAdder
+from src.Repository.db_connection import DbConnection
+from src.Repository.user_name_repository import UserNameRepository
+import pytest
+from unittest.mock import Mock, MagicMock
+
+def test_that_adder_adds_friends():
+    # Arrange
+    repo = Mock(UserNameRepository)
+    connection = Mock(DbConnection)
+    validator = Mock()
+    adder = FriendAdder(repo, connection, validator)
+    # Act
+    adder.add_friend("stever", "gerald")
+    # Assert
+    assert repo.add_friend.called == True
+
