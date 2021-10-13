d6351159061c23a1d91b1c35b3c749e34fae41f8
Toggi <thorgilshjalmarsson@gmail.com>
2021-10-13 08:50:03 +0000

    Users can now add friends!

--- src/player.py
+++ src/player.py
@@ -1,13 +1,15 @@
 
 from src.Players.players_list import PlayersList
 from src.message_sender import MessageSender
-
+from src.friend_adder import FriendAdder
 class Player:
-    def __init__(self, message_sender: MessageSender, player_list: PlayersList):
+    def __init__(self, message_sender: MessageSender, player_list: PlayersList, friend_adder: FriendAdder):
         self.__message_sender = message_sender
         self.__player_list = player_list
+        self.__friend_adder = friend_adder
+
     def add_friend(self, username, friend_username):
-        pass
-        #TODO: here
+        self.__friend_adder.add_friend(username, friend_username)
+
     def send_message(self, friend_username, message):
         self.__message_sender.send(friend_username, message)
